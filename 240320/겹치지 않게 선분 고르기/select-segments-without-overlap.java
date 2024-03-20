805699147f2cd8733337003b3323f46334b9f92c
Hypersand <on8214@gmail.com>
2024-03-20 14:22:45 +0900

    Title: 겹치지 않게 선분 고르기, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240320/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -0,0 +1,75 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static List<Node> lines = new ArrayList<>();
+    private static List<Node> useLines = new ArrayList<>();
+    private static int n;
+    private static int answer = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        for (int i = 0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            lines.add(new Node(x, y));
+        }
+
+        for (int i = n; i >= 1; i--) {
+            int tmp = answer;
+            comb(0, i);
+
+            if (tmp != answer) {
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static void comb(int idx, int maxSize) {
+        if (idx == maxSize) {
+            if (validate()) {
+                answer = maxSize;
+                System.out.println(answer);
+                System.exit(0);
+            }
+            return;
+        }
+
+        for (int i = idx; i<n; i++) {
+            useLines.add(lines.get(i));
+            comb(i + 1, maxSize);
+            useLines.remove(useLines.size() - 1);
+        }
+    }
+
+    //선택한 선분이 겹치는지 검증한다.
+    private static boolean validate() {
+        for (int i = 0; i<useLines.size() - 1; i++) {
+            Node node1 = useLines.get(i);
+            for (int j = i + 1; j<useLines.size(); j++) {
+                Node node2 = useLines.get(j);
+                if (node1.x <= node2.x && node1.y >= node2.y) {
+                    return false;
+                }
+
+                if (node2.x <= node1.x && node2.y >= node1.y) {
+                    return false;
+                }
+             }
+        }
+        return true;
+    }
+
+    private static class Node {
+        private int x;
+        private int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
