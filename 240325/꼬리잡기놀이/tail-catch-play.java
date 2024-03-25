cc4bc670cfb42befd27226bfa271e9f5af052018
Hypersand <on8214@gmail.com>
2024-03-25 19:26:42 +0900

    Title: 꼬리잡기놀이, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240325/꼬리잡기놀이/tail-catch-play.java
+++ 240325/꼬리잡기놀이/tail-catch-play.java
@@ -9,6 +9,7 @@ public class Main {
     private static int[] dx = {-1, 1, 0, 0}; //상, 하
     private static int[] dy = {0, 0, -1, 1}; //좌, 우
     private static boolean[][] visited;
+    private static List<Node> list;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -37,7 +38,55 @@ public class Main {
 
     private static void playGame(int round) {
         //1. 각 팀은 머리사람을 따라서 한 칸 이동한다.
-        move();
+        visited = new boolean[n][n];
+        while (!queue.isEmpty()) {
+            list = new ArrayList<>();
+            Node head = queue.poll();
+            list.add(head);
+            visited[head.x][head.y] = true;
+            move(head.x, head.y, 0);
+            
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i<4; i++) {
+                int nx = head.x + dx[i];
+                int ny = head.y + dy[i];
+                if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+                if (map[nx][ny] == 4 || map[nx][ny] == 3) {
+                    x = nx;
+                    y = ny;
+                    break;
+                }
+            }
+
+            int tmp = map[x][y];
+            for (int i = 0; i<list.size() - 1; i++) {
+                Node node = list.get(i);
+                System.out.println(x + " " + y + " ----" + map[node.x][node.y]);
+                map[x][y] = map[node.x][node.y];
+                x = node.x;
+                y = node.y;
+
+                if (tmp == 3 && i == list.size() - 2) {
+                    map[x][y] = 3;
+                }
+            }
+
+            Node tmpNode = list.get(list.size() - 1);
+            if (tmp == 4) {
+                map[tmpNode.x][tmpNode.y] = 3;
+            } else {
+                map[tmpNode.x][tmpNode.y] = 1;
+            }
+        }
+
+        for (int i = 0; i<n; i++) {
+            for (int j = 0; j<n; j++) {
+                System.out.print(map[i][j]);
+            }
+            System.out.println();
+        }
+
 
         //2. 라운드에 맞게 공이 정해진 선을 따라서 던져진다.
         throwBall(round);
@@ -53,44 +102,20 @@ public class Main {
 
     }
 
-    private static void move() {
-        visited = new boolean[n][n];
-        while(!queue.isEmpty()) {
-            Node node = queue.poll();
-            for (int i = 0; i<4; i++) {
-                int nx = node.x + dx[i];
-                int ny = node.y + dy[i];
-                if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-                if (map[nx][ny] == 0) continue;
-                //4라면 이동해야할 칸
-                //머리 이동
-                if (map[nx][ny] == 4) {
-                    map[nx][ny] = 1;
-                    map[node.x][node.y] = 4;
-                    //몸통, 꼬리 이동
-                    visited[nx][ny] = true;
-                    moveBodyAndTail(node.x, node.y);
-                }
-            }
-        }
-    }
-
-    //x, y : 이동할 위치
-    private static void moveBodyAndTail(int x, int y) {
+    private static void move(int x, int y, int idx) {
         for (int i = 0; i<4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (nx < 0 || ny < 0 || nx >= n || ny >=n) continue;
-            if (map[nx][ny] == 0) continue;
+            if (nx < 0 || ny < 0 || nx >=n || ny >=n) continue;
+            if (map[nx][ny] == 0 || map[nx][ny] == 4) continue;
             if (visited[nx][ny]) continue;
-            if (map[nx][ny] == 2 || map[nx][ny] == 3) {
-                map[x][y] = map[nx][ny];
-                map[nx][ny] = 4;
-                visited[x][y] = true;
-                moveBodyAndTail(nx, ny);
-            }
-        }
+            if (idx == 0 && map[nx][ny] != 2) continue;
+            list.add(new Node(nx, ny));
+            visited[nx][ny] = true;
+            move(nx, ny, idx + 1);
+        }       
     }
+
 
     private static void throwBall(int round) {
         round = round % (4 * n);
